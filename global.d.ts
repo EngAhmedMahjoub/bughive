bd0a240bbcf0b2915e074a34ccc461b600cbf959
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2026-04-12 16:26:31 +1000

    fix package compatibilities

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,17 @@
 declare module "*.css";
+
+// Compatibility shim for dependencies that still use the legacy global JSX namespace.
+declare namespace JSX {
+  type ElementType = import("react").JSX.ElementType;
+  interface Element extends import("react").JSX.Element {}
+  interface ElementClass extends import("react").JSX.ElementClass {}
+  interface ElementAttributesProperty
+    extends import("react").JSX.ElementAttributesProperty {}
+  interface ElementChildrenAttribute
+    extends import("react").JSX.ElementChildrenAttribute {}
+  type LibraryManagedAttributes<C, P> = import("react").JSX.LibraryManagedAttributes<C, P>;
+  interface IntrinsicAttributes extends import("react").JSX.IntrinsicAttributes {}
+  interface IntrinsicClassAttributes<T>
+    extends import("react").JSX.IntrinsicClassAttributes<T> {}
+  interface IntrinsicElements extends import("react").JSX.IntrinsicElements {}
+}
