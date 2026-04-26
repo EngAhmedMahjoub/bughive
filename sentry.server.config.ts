f54c298a8f2945ba261d2d00289ae03878fee21e
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2026-04-26 10:52:47 +1000

    set up error tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a569324a9a617ff0d3320eb03dd15c74@o4511280988815361.ingest.de.sentry.io/4511280997007440",
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
